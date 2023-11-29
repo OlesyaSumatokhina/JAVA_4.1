5217a7e6d9f1ae5e79c28502ee66bd83676933b2
Olesya Sumatokhina <olesya_po@mail.ru>
2023-11-29 10:17:06 +0300

    changereturn

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,5 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int result;
-        result = price / 20;
-        return result;
+        return price / 20;
     }
 }
